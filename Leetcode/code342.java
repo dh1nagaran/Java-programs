cb70deea2865d3182f275b8597a0c09aec65e075
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-01-07 00:18:28 +0530

    Add files via upload

---
+++ Leetcode/code342.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class code
+{
+	public static void main(String[] args)
+	{
+		if(1073741824/16==0)
+		{
+			System.out.println(true);
+			
+		}
+		else
+			System.out.println(false);
+	}
+}
