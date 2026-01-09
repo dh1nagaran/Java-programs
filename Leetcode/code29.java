13b839829eb6e1cf7cb86108d4631846c850d7e5
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-01-09 15:14:09 +0530

    Add files via upload

---
+++ Leetcode/code29.java
@@ -0,0 +1,9 @@
+class code
+{
+	public static void main(String[] args)
+	{
+		int dividend=Integer.MIN_VALUE;
+		int divisor=-1;
+		return dividend==Interger.MIN_VALUE && divisor==-1?Integer.MAX_VALUE:dividend/divisor;
+	}
+}
