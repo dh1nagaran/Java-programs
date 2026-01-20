e78c502519cad5b8303092f8f06fe41b932d2703
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-01-20 22:43:21 +0530

    Add files via upload

---
+++ Leetcode/code190.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class code
+{
+		
+	public static void main(String[] args)
+	{
+		int n=43261596;
+		System.out.println(Integer.toBinaryString(n));
+		String data=Integer.toBinaryString(n);
+		String data2=String.format("%32s", data).replaceAll(" ", "0");
+		System.out.println(data);s
+		String neww="";
+		int i=data2.length()-1;
+		while(i>=0)
+		{
+			neww+=data2.charAt(i);
+			i--;
+		}
+		int num=Integer.parseInt(neww,2);
+		System.out.println(num);
+	}
+}
