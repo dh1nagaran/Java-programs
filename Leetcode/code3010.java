6935a9054f237649a6b8f13c8dbe334c4e806c16
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-02-01 22:12:39 +0530

    Add files via upload

---
+++ Leetcode/code3010.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class code3010
+{
+	public static void main(String args[])
+	{
+		int nums[]={5,4,3};
+		int first=nums[0];
+		int min1=Integer.MAX_VALUE;
+		int min2=Integer.MAX_VALUE;
+		for (int i = 1; i < nums.length; i++) 
+		{
+            if (nums[i] < min1) 
+			{
+                min2 = min1;
+                min1 = nums[i];
+			} 
+			else if (nums[i] < min2) 
+			{
+                min2 = nums[i];
+            }
+        }
+		int num=first+min1+min2;
+		System.out.println(num);
+		
+		
+	}
+
+}	
