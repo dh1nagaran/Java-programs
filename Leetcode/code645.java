50f0cd509a79a06b1e51ff0659cec448f1ad10cd
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-03-27 12:22:52 +0530

    Add files via upload

---
+++ Leetcode/code645.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int res[] = new int[2];
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hs.contains(nums[i]))
+                res[0] = nums[i];
+            hs.add(nums[i]);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (!hs.contains(i))
+                res[1] = i;
+        }
+        return res;
+
+    }
+}
