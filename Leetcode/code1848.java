3252aa9084da774157bb9978cad532d2c2571600
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-04-13 08:18:44 +0530

    Add files via upload

---
+++ Leetcode/code1848.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int n=nums.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==target)
+            {
+                int dis=Math.abs(i-start);
+                min=Math.min(min,dis);
+            }
+        }
+        return min;
+        
+    }
+}
