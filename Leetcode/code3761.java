a1161a3c080189ea3efe61d946311dfe64e5bc1b
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-04-19 13:32:39 +0530

    Update code3761.java

--- Leetcode/code3761.java
+++ Leetcode/code3761.java
@@ -1,21 +1,12 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            map.putIfAbsent(nums[i], new ArrayList<>());
-            map.get(nums[i]).add(i);
-        }
-        for (int i = 0; i < nums.length - 1; i++) {
-            int rev = reverse(nums[i]);
-            List<Integer> ls = map.get(rev);
-            if (ls == null)
-                continue;
-            int pos = Collections.binarySearch(ls, i + 1);
-            if (pos < 0)
-                pos = -(pos + 1);
-            if (pos < ls.size())
-                min = Math.min(min, ls.get(pos) - i);
+        for(int i=0;i<nums.length;i++)
+        {
+            int num=nums[i];
+            if(map.containsKey(nums[i])) min=Math.min(min,i-map.get(num));
+            map.put(reverse(nums[i]),i);
         }
         return min == Integer.MAX_VALUE ? -1 : min;
     }
