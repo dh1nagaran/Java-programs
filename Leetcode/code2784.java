ef42d97109e695df7f6be052e7e527ede4f73772
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-05-14 14:42:21 +0530

    Add files via upload

---
+++ Leetcode/code2784.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if(n<nums[n-1] || n==1) return false;
+       for(int i=0;i<n-1;i++)
+       {
+            if(nums[i]!=i+1) return false;
+       }
+        if(nums[n-1]!=nums[n-2]) return false;
+        return true;
+    }
+}
