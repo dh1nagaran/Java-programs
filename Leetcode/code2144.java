3e59188b4231563b914582b1b0716aeb7b9106d3
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-06-01 09:51:59 +0530

    Add files via upload

---
+++ Leetcode/code2144.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int sum=0;
+        int count=0;
+        for(int i=cost.length-1;i>=0;i--)
+        {
+            count++;
+            if(count==3)count=0;
+            else sum+=cost[i];
+        }
+        return sum;
+        
+    }
+}
