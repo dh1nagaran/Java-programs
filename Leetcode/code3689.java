60a74d55889f0233675e46a5d85fd267ae4fc682
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-06-09 10:07:54 +0530

    Create code3689.java

---
+++ Leetcode/code3689.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+      int max=Integer.MIN_VALUE;   
+      int min=Integer.MAX_VALUE;
+      for(int n:nums)
+      {
+            min=Math.min(min,n);
+            max=Math.max(max,n);
+      }   
+      return (long)(max-min)*k;
+    }
+}
