39bc7df47286cabd874a6901db3005472bf8d30a
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-06-15 08:01:49 +0530

    Add files via upload

---
+++ Leetcode/code922.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+           List<Integer> pos=new ArrayList<>();
+        List<Integer> neg=new ArrayList<>();
+        int n=nums.length;
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]%2==0)pos.add(nums[i]);
+            else neg.add(nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)nums[i]=pos.get(j);
+            else
+            {
+                nums[i]=neg.get(j);
+                j++;
+            }
+        }
+        return nums;
+        
+    }
+}
