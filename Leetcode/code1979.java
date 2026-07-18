09298f40f1a66c4a6c3be040c0993fe45b57b3bb
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-07-18 16:13:25 +0530

    Add files via upload

---
+++ Leetcode/code1979.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=nums[0];i>=1;i--)
+        {
+            if(nums[0]%i==0 && nums[nums.length-1]%i==0)return i;
+        }
+        return 1;
+    }
+}
