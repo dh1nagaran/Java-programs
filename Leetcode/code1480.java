d65e4efdffae8cb76173113c83edfddeac29ea2e
Dhinagaran <124670153+dh1nagaran@users.noreply.github.com>
2026-08-25 22:01:33 +0530

    Add files via upload

---
+++ Leetcode/code1480.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int prev=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            prev+=nums[i];
+            nums[i]=prev;
+        }
+        return nums;
+        
+    }
+}
